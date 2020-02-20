39d0c57d3f3c4f41d614228f61adc45cdcbe4633
Janjs <jan.jime.serra@gmail.com>
2020-02-20 13:35:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Django VueJs Template</h1>
+    <div id="nav">
+     <router-link :to="{ name: 'home' }">Vue</router-link> |
+     <router-link :to="{ name: 'messages' }">Django Rest</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+ background-image: url("./assets/flagbackground.gif");
+ background-color: #2c3e50;
+ background-repeat: no-repeat;
+ background-size: cover;
+ background-position: center;
+}
+</style>
